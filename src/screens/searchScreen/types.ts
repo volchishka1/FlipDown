3dd7a714a293c31d274c85d6d7fdb48fa90ab189
macwolf <romazverb18@gmsil.com>
2024-04-23 17:21:04 +0300

    added some types in searchScreen

--- src/screens/searchScreen/types.ts
+++ src/screens/searchScreen/types.ts
@@ -2,5 +2,7 @@ import { TextInputProps, TextProps } from 'react-native';
 
 export type SearchScreenProps = {
   link: TextProps['children'];
+  text: TextProps['children'];
   setInputValue: TextInputProps['onChangeText'];
+  setLink: TextInputProps['onChangeText'];
 };
