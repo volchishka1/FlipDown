66cf0b77e0b02610fcce0e7628067994f820b5a5
macwolf <romazverb18@gmsil.com>
2024-06-17 22:57:24 +0300

    added some styles and components to downloadScreenView

--- src/screens/downloadScreen/downloadScreenView.tsx
+++ src/screens/downloadScreen/downloadScreenView.tsx
@@ -1,6 +1,10 @@
-import { Image, SafeAreaView } from 'react-native';
+import { Image, SafeAreaView, Text, View } from 'react-native';
 
 import React, { FC } from 'react';
+
+import { ScrollView } from 'react-native-gesture-handler';
+
+import { CustomButton } from '../../components/buttonComponent/customButtonComponent';
 
 import { downloadScreenStyles } from './styles';
 import { DownloadScreenProps } from './types';
@@ -8,10 +12,34 @@ import { DownloadScreenProps } from './types';
 export const DownloadScreenView: FC<DownloadScreenProps> = (props) => {
   const { photos } = props;
   return (
-    <SafeAreaView style={downloadScreenStyles.rootContainer}>
-      {photos.map((obj, index) => (
-        <Image key={index} style={{ width: '20%', height: '10%' }} source={obj.node.image} />
-      ))}
+    <SafeAreaView style={downloadScreenStyles.saveAriaView}>
+      <ScrollView style={downloadScreenStyles.rootContainer}>
+        {photos.map((img) => (
+          <View style={downloadScreenStyles.centerContainer}>
+            <View style={downloadScreenStyles.itemContainer}>
+              <View style={downloadScreenStyles.imageContainer}>
+                <Image
+                  key={img?.node?.image?.uri}
+                  style={downloadScreenStyles.images}
+                  source={img.node.image}
+                />
+                <View style={downloadScreenStyles.titleContainer}>
+                  <Text key={img.node.image.uri} style={downloadScreenStyles.title}>
+                    {img.node?.type}
+                  </Text>
+                </View>
+              </View>
+              <View style={downloadScreenStyles.buttonContainer}>
+                <CustomButton
+                  style={downloadScreenStyles.button}
+                  textButton={'Delete'}
+                  isLoad={false}
+                />
+              </View>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
