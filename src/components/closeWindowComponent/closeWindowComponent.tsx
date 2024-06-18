f03507c30cded9363312760f16edb6755435c922
macwolf <romazverb18@gmsil.com>
2024-06-18 23:29:21 +0300

    added style to closeModalWindow

--- src/components/closeWindowComponent/closeWindowComponent.tsx
+++ src/components/closeWindowComponent/closeWindowComponent.tsx
@@ -4,6 +4,8 @@ import React, { FC } from 'react';
 
 import { CloseWindowSvg } from '../../../assets/closeModalWindow';
 
+import { closeWindowStyle } from './styles';
+
 export type CloseWindowComponentProps = {
   goToCloseBottomSheet: () => void;
 };
@@ -11,8 +13,12 @@ export type CloseWindowComponentProps = {
 export const CloseWindowComponent: FC<CloseWindowComponentProps> = (props) => {
   const { goToCloseBottomSheet } = props;
   return (
-    <View>
-      <TouchableOpacity accessibilityRole={'button'} onPress={goToCloseBottomSheet}>
+    <View style={closeWindowStyle.rootContainer}>
+      <TouchableOpacity
+        style={closeWindowStyle.buttonStyle}
+        accessibilityRole={'button'}
+        onPress={goToCloseBottomSheet}
+      >
         <CloseWindowSvg />
       </TouchableOpacity>
     </View>
