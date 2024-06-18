c447aaa02437385345b69e0a82a3d0bbb53e7f23
macwolf <romazverb18@gmsil.com>
2024-06-18 23:31:14 +0300

    added new styles to infoScreen

--- src/screens/personalInfoScreen/styles.ts
+++ src/screens/personalInfoScreen/styles.ts
@@ -1,9 +1,24 @@
-import { StyleSheet, ViewStyle } from 'react-native';
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 export type Styles = {
   rootContainer: ViewStyle;
+  centerContainer: ViewStyle;
+  textStyle: TextStyle;
 };
 
 export const infoScreenStyles = StyleSheet.create<Styles>({
-  rootContainer: { backgroundColor: '#1d014b', flex: 1 },
+  rootContainer: {
+    backgroundColor: '#1d014b',
+    flex: 1,
+  },
+  centerContainer: {
+    justifyContent: 'space-between',
+    marginTop: '15%',
+    height: '30%',
+    marginHorizontal: '2%',
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 20,
+  },
 });
