071626f1e5b3eda0ba9fd5a846bf47e0bcb5994f
macwolf <romazverb18@gmsil.com>
2024-06-19 23:07:47 +0300

    refactored styles in infoScreenStyle

--- src/screens/personalInfoScreen/styles.ts
+++ src/screens/personalInfoScreen/styles.ts
@@ -3,19 +3,28 @@ import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 export type Styles = {
   rootContainer: ViewStyle;
   centerContainer: ViewStyle;
+  bottomContainer: ViewStyle;
   textStyle: TextStyle;
+  textDescriptionContainer: ViewStyle;
 };
 
 export const infoScreenStyles = StyleSheet.create<Styles>({
   rootContainer: {
     backgroundColor: '#1d014b',
     flex: 1,
+    justifyContent: 'space-between',
   },
   centerContainer: {
     justifyContent: 'space-between',
     marginTop: '15%',
     height: '25%',
     marginHorizontal: '3%',
+  },
+  bottomContainer: {
+    alignItems: 'center',
+  },
+  textDescriptionContainer: {
+    marginTop: '5%',
   },
   textStyle: {
     fontFamily: 'Roboto-Bold',
