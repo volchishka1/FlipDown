c18fec9aaf8fffe08003fb899a473e78e6b53181
macwolf <romazverb18@gmsil.com>
2024-06-20 15:50:08 +0300

    refactored contactsScreenView text

--- src/screens/contactsScreen/contactsScreenView.tsx
+++ src/screens/contactsScreen/contactsScreenView.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import React, { FC } from 'react';
 
 import { ScrollView } from 'react-native-gesture-handler';
 
 import { CloseWindowComponent } from '../../components/closeWindowComponent/closeWindowComponent';
+import { ParagraphComponent } from '../../components/textComponent/paragraphComponent';
+import { TextComponent } from '../../components/textComponent/textComponent';
 
 import { contactScreenStyles } from './styles';
 
@@ -16,16 +18,18 @@ export const ContactsScreenView: FC<ContactsScreenProps> = ({ goToCloseBottomShe
     <View style={contactScreenStyles.rootContainer}>
       <CloseWindowComponent goToCloseBottomSheet={goToCloseBottomSheet} />
       <ScrollView style={contactScreenStyles.centerContainer}>
-        <Text>
-          {'Contact us\n' +
+        <ParagraphComponent paragraphName={'Contact us'} />
+        <TextComponent
+          text={
             'You can contact us using the following contact data:\n' +
             '\n' +
             'Email: fliptokapp@gmail.com\n' +
             '\n' +
             'If you have questions, suggestions or comments, please do not hesitate to contact us. We are always ready to help you and answer all your questions.\n' +
             '\n' +
-            'We value your attention to our site and are always ready to help you. Regardless of questions or requests, you can be sure that you will receive our professional and friendly support.'}
-        </Text>
+            'We value your attention to our site and are always ready to help you. Regardless of questions or requests, you can be sure that you will receive our professional and friendly support.'
+          }
+        />
       </ScrollView>
     </View>
   );
