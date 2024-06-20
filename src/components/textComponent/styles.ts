7949de8145faf1c323cc675b04171db9549c33dd
macwolf <romazverb18@gmsil.com>
2024-06-20 15:50:32 +0300

    added fontSize to textComponent

--- src/components/textComponent/styles.ts
+++ src/components/textComponent/styles.ts
@@ -15,10 +15,12 @@ export const textStyle = StyleSheet.create<TextStyleProps>({
     marginBottom: 30,
   },
   textStyle: {
+    fontSize: 14,
     fontFamily: 'Roboto',
     color: '#ffffff',
   },
   paragraphStyle: {
+    fontSize: 22,
     fontFamily: 'Roboto-Medium',
     color: '#c9c9c9',
   },
