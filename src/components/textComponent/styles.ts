96cd3618c50ee4bc941e5225beed5557715e44bb
macwolf <romazverb18@gmsil.com>
2024-06-21 15:54:33 +0300

    refactored styles in textComponent

--- src/components/textComponent/styles.ts
+++ src/components/textComponent/styles.ts
@@ -3,8 +3,10 @@ import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 export type TextStyleProps = {
   rootParagraphContainer: ViewStyle;
   rootTextContainer: ViewStyle;
+  rootHeaderContainer: ViewStyle;
   textStyle: TextStyle;
   paragraphStyle: TextStyle;
+  headerStyle: TextStyle;
 };
 
 export const textStyle = StyleSheet.create<TextStyleProps>({
@@ -12,15 +14,25 @@ export const textStyle = StyleSheet.create<TextStyleProps>({
     marginBottom: 10,
   },
   rootTextContainer: {
-    marginBottom: 30,
+    marginTop: 10,
+    // backgroundColor: 'blue',
+  },
+  rootHeaderContainer: {
+    marginBottom: 10,
   },
   textStyle: {
     fontSize: 14,
     fontFamily: 'Roboto',
     color: '#ffffff',
+    lineHeight: 18,
   },
   paragraphStyle: {
     fontSize: 22,
+    fontFamily: 'Roboto-Medium',
+    color: '#c9c9c9',
+  },
+  headerStyle: {
+    fontSize: 18,
     fontFamily: 'Roboto-Medium',
     color: '#c9c9c9',
   },
