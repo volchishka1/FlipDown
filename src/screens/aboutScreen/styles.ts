32b0985ad11bbafaa2aa38681b61116532471705
macwolf <romazverb18@gmsil.com>
2024-06-23 23:30:16 +0300

    refactored styles on aboutScreen

--- src/screens/aboutScreen/styles.ts
+++ src/screens/aboutScreen/styles.ts
@@ -1,16 +1,25 @@
-import { StyleSheet, ViewStyle } from 'react-native';
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 export type AboutScreenStyles = {
   rootContainer: ViewStyle;
   centerContainer: ViewStyle;
+  dotComponent: ViewStyle;
+  textStyle: TextStyle;
 };
 
 export const aboutScreenStyles = StyleSheet.create<AboutScreenStyles>({
   rootContainer: {
     backgroundColor: '#8c54ea',
+    marginBottom: 10,
   },
   centerContainer: {
     marginHorizontal: '3%',
     marginBottom: '10%',
+  },
+  dotComponent: {
+    marginBottom: 10,
+  },
+  textStyle: {
+    marginBottom: 10,
   },
 });
