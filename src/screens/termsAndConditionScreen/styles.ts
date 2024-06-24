e587b54dd01bd92c637391390292a1d10693b08b
macwolf <romazverb18@gmsil.com>
2024-06-24 17:53:44 +0300

    added new style in termsAndCondStyles

--- src/screens/termsAndConditionScreen/styles.ts
+++ src/screens/termsAndConditionScreen/styles.ts
@@ -1,16 +1,29 @@
-import { StyleSheet, ViewStyle } from 'react-native';
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 export type TermsAndConditionStyles = {
   rootContainer: ViewStyle;
   centerContainer: ViewStyle;
+  descriptionUpdated: TextStyle;
+  descriptionText: TextStyle;
+  paragraphText: TextStyle;
 };
 
 export const termsAndConditionsStyles = StyleSheet.create<TermsAndConditionStyles>({
   rootContainer: {
     backgroundColor: '#8c54ea',
+    marginBottom: 20,
   },
   centerContainer: {
     marginHorizontal: '3%',
     marginBottom: '10%',
+  },
+  descriptionUpdated: {
+    marginBottom: 40,
+  },
+  descriptionText: {
+    marginBottom: 10,
+  },
+  paragraphText: {
+    fontSize: 18,
   },
 });
