f93fc94be4964d2de22f371c250ec872e61dc05f
macwolf <romazverb18@gmsil.com>
2024-06-24 22:52:43 +0300

    added Styleprops to textComponent

--- src/components/textComponent/textComponent.tsx
+++ src/components/textComponent/textComponent.tsx
@@ -7,13 +7,14 @@ import { textStyle } from './styles';
 export interface TextComponentProps {
   text?: string;
   style?: any;
+  textStyles?: any;
 }
 
 export const TextComponent: FC<TextComponentProps> = (props) => {
-  const { text, style } = props;
+  const { text, style, textStyles } = props;
   return (
     <View style={[textStyle.rootTextContainer, style]}>
-      <Text style={textStyle.textStyle}>{text}</Text>
+      <Text style={[textStyle.textStyle, textStyles]}>{text}</Text>
     </View>
   );
 };
