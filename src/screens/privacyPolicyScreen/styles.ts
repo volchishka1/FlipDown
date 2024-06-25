e5cdf5d6198550d25262d8b57822bbb47ec23623
macwolf <romazverb18@gmsil.com>
2024-06-25 21:49:07 +0300

    refactored styles privacyPolicyScreenStyles

--- src/screens/privacyPolicyScreen/styles.ts
+++ src/screens/privacyPolicyScreen/styles.ts
@@ -8,7 +8,7 @@ export type PrivacyPolicyScreenStyles = {
 export const privacyPolicyScreenStyles = StyleSheet.create<PrivacyPolicyScreenStyles>({
   rootContainer: {
     backgroundColor: '#8c54ea',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   centerContainer: {
     marginHorizontal: '3%',
