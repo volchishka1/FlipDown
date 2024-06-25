5cd5683061e554343a6746ad1b60637d3d945fe4
macwolf <romazverb18@gmsil.com>
2024-06-25 22:47:16 +0300

    added keyboardAwareScrollView to searchScreenView

--- src/screens/searchScreen/searchScreenView.tsx
+++ src/screens/searchScreen/searchScreenView.tsx
@@ -7,6 +7,7 @@ import { LogoComponent } from '../../components/logoComponent/logoComponent';
 
 import { searchScreenStyles } from './styles';
 import { SearchScreenProps } from './types';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export const SearchScreenView: FC<SearchScreenProps> = (props) => {
   const {
@@ -21,39 +22,41 @@ export const SearchScreenView: FC<SearchScreenProps> = (props) => {
   } = props;
   return (
     <SafeAreaView style={searchScreenStyles.rootContainer}>
-      <View style={searchScreenStyles.topContainer}>
-        <LogoComponent />
-      </View>
-      <View style={searchScreenStyles.inputContainer}>
-        <TextInput
-          value={link}
-          onChangeText={setLink}
-          accessibilityLabel={'Text input field'}
-          style={searchScreenStyles.input}
-        />
-        <CustomButton onPress={setInputValue} textButton={'Download'} isLoad={isLoad} />
-      </View>
-      <View style={searchScreenStyles.bottomContainer}>
-        {showLoad && (
-          <View style={searchScreenStyles.dataContainer}>
-            <Image style={searchScreenStyles.imageStyle} source={{ uri: `${preview}` }} />
-            <View style={searchScreenStyles.buttonDataContainer}>
-              <CustomButton
-                onPress={saveVideo}
-                textButton={'Download video'}
-                style={searchScreenStyles.buttonStyle}
-                isLoad={false}
-              />
-              <CustomButton
-                onPress={saveMusic}
-                textButton={'Download mp3'}
-                style={searchScreenStyles.buttonStyle}
-                isLoad={false}
-              />
+      <KeyboardAwareScrollView scrollEnabled={false} style={{ marginTop: '30%' }}>
+        <View style={searchScreenStyles.topContainer}>
+          <LogoComponent />
+        </View>
+        <View style={searchScreenStyles.inputContainer}>
+          <TextInput
+            value={link}
+            onChangeText={setLink}
+            accessibilityLabel={'Text input field'}
+            style={searchScreenStyles.input}
+          />
+          <CustomButton onPress={setInputValue} textButton={'Download'} isLoad={isLoad} />
+        </View>
+        <View style={searchScreenStyles.bottomContainer}>
+          {showLoad && (
+            <View style={searchScreenStyles.dataContainer}>
+              <Image style={searchScreenStyles.imageStyle} source={{ uri: `${preview}` }} />
+              <View style={searchScreenStyles.buttonDataContainer}>
+                <CustomButton
+                  onPress={saveVideo}
+                  textButton={'Download video'}
+                  style={searchScreenStyles.buttonStyle}
+                  isLoad={false}
+                />
+                <CustomButton
+                  onPress={saveMusic}
+                  textButton={'Download mp3'}
+                  style={searchScreenStyles.buttonStyle}
+                  isLoad={false}
+                />
+              </View>
             </View>
-          </View>
-        )}
-      </View>
+          )}
+        </View>
+      </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 };
