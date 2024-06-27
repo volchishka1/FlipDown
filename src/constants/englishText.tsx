cb8367e78ea5f65b95c2b9fdfa19b0ae9648ee76
macwolf <romazverb18@gmsil.com>
2024-06-28 00:02:17 +0300

    added englishConst

---
+++ src/constants/englishText.tsx
@@ -0,0 +1,42 @@
+export const DOWNLOAD = 'Download';
+export const SEARCH = 'Search';
+export const INFORMATION = 'Information';
+export const ABOUT = 'About';
+export const CONTACTS = 'Contacts';
+export const PRIVACY_POLICY = 'Privacy policy';
+export const TERMS_AND_CONDITIONS = 'Terms and conditions';
+export const DOWNLOAD_VIDEO = 'Download video';
+export const DOWNLOAD_MP3 = 'Download mp3';
+
+// ABOUT STRINGS
+export const ABOUT_PROJECT = 'What is the FlipTok Project?';
+export const IF_YOU_NEED = 'If you need to download videos from TikTok and you do not know how to do it, we advise you to pay attention to the FlipTok project. FlipTok is a website that allows you to deal with the task in just a few seconds. The service is characterized by a simple and clear interface, which means that it allows you to find and download any videos from TikTok, and will be easy to understand for teenagers and older users. There will be no difficulties because we offer the best solution to save clips on any device.';
+export const HOW_TO_USE = 'How to use the service?';
+export const WORKING_WITH = 'Working with the application involves very simple steps. To download videos and music from TikTok, you only need a few steps. All you need is this:';
+export const GO_TO_THE_APP = 'go to the app and find a clip you like;';
+export const COPY_LINK = 'copy the link using the "Share" button;';
+export const OPEN_THE_APP = 'open the FlipTok application and paste the copied link into the input field.';
+export const THE_FINAL_STEP = 'The final step is to click the "Download" button and wait for the video to download. It will be found in the "Downloads" or "My Files" folder. Please note that only FlipTok allows you to download videos from TikTok without a watermark, other similar services can\'t do this.\n' +
+  '\n' +
+  'On this website, you can download not only short video clips but also music from TikTok. The algorithm of actions does not change. It is just as easy and simple.\n'
+export const BENEFITS = 'Benefits of FlipTok. What should a user know?';
+export const IF_YOU_CHOSEN = 'If you have chosen FlipTok for downloading files, you definitely have not made a mistake, because the service has a lot of advantages. The main pros are:'
+export const NO_USER_FEES = 'no user fees. You really do not have to pay when downloading clips from TikTok without a watermark, even if there is a lot of content and its size is quite large;'
+export const HIGH_QUALITY = 'high-quality videos. FlipTok allows you to download clips without loss of quality, which means you can watch them on your device in the best possible way;'
+export const ANY_FORMAT = 'any video format. We can download videos in MP4, AVI, 3GP and other formats without any problems. In any case, the download will be as fast as possible and the clip will be of high quality;';
+export const CONFIDENTIALITY = 'confidentiality. FlipTok will not save user data even if you open the site daily and download a lot of content.'
+export const USING_THE_FLIPTOK = 'Using the FlipTok website is not difficult at all, and there is no need to register and authorize. There is no need to download additional extensions, plugins, add-ons, because the website works in any browser.\n' +
+  '\n' +
+  'Another important advantage of the website is the ability to download content to any device. It can be a tablet, a smartphone, or a laptop, in other words, any device on which you usually watch videos.\n' +
+  '\n' +
+  'Downloading is fast, literally in a few seconds for standard-sized videos up to 60 seconds long. Longer videos will take longer to save, but the quality will remain original.\n' +
+  '\n' +
+  'Are you ready to get acquainted with FlipTok service? Hurry up and enjoy videos from TikTok without using the app and internet connection. Our service has already been chosen by hundreds of users of the popular application. It is really the easiest and most convenient way to download clips without watermarking and registration that you and your loved ones are sure to enjoy!';
+
+// CONTACTS STRINGS
+
+export const CONTACT_US = 'Contact us';
+export const YOU_CAN_CONTACT_US = 'You can contact us using the following contact data:';
+export const EMAIL = 'Email: fliptokapp@gmail.com';
+export const IF_YOU_HAVE_QUESTIONS = 'If you have questions, suggestions or comments, please do not hesitate to contact us. We are always ready to help you and answer all your questions.';
+export const WE_VALUE_YOUR_ATTENTION = 'We value your attention to our site and are always ready to help you. Regardless of questions or requests, you can be sure that you will receive our professional and friendly support.';
