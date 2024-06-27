b3a586257a0486dace0d167f73f4969a3a78c287
macwolf <romazverb18@gmsil.com>
2024-06-28 00:00:18 +0300

    added path folders to babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
+const root = path.resolve(__dirname);
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
@@ -5,7 +9,18 @@ module.exports = {
     [
       'module-resolver',
       {
+        alias: {
+          '@assets': `${root}/assets`,
+          '@components': `${root}/src/components`,
+          '@config': `${root}/src/config`,
+          '@constants': `${root}/src/constants`,
+          '@navigation': `${root}/src/navigation`,
+          '@root': `${root}/src`,
+          '@screens': `${root}/src/screens`,
+          '@state': `${root}/src/state`,
+        },
         extensions: ['.android.js', '.ios.js', '.js', '.json', '.ts', '.tsx'],
+        root: [root],
       },
     ],
     'react-native-reanimated/plugin',
