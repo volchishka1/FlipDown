4a180d3715ebe357ff7b73c94222c7a3c21850c7
macwolf <romazverb18@gmsil.com>
2024-06-28 00:00:39 +0300

    added another language to aboutScreen

--- src/screens/aboutScreen/aboutScreenView.tsx
+++ src/screens/aboutScreen/aboutScreenView.tsx
@@ -4,13 +4,14 @@ import React, { FC } from 'react';
 
 import { ScrollView } from 'react-native-gesture-handler';
 
-import { CloseWindowComponent } from '../../components/closeWindowComponent/closeWindowComponent';
-import { DotComponent } from '../../components/dotComponent/dotComponent';
-import { ParagraphComponent } from '../../components/textComponent/paragraphComponent';
-import { TextComponent } from '../../components/textComponent/textComponent';
+import { CloseWindowComponent } from '@components/closeWindowComponent/closeWindowComponent';
+import { DotComponent } from '@components/dotComponent/dotComponent';
+import { ParagraphComponent } from '@components/textComponent/paragraphComponent';
+import { TextComponent } from '@components/textComponent/textComponent';
 
 import { aboutScreenStyles } from './styles';
-import { globalStyles } from '../../components/globalStyles/globalStyles';
+import { globalStyles } from '@components/globalStyles/globalStyles';
+import { strings } from "@constants/textConst";
 
 export type AboutScreenProps = {
   goToCloseBottomSheet: () => void;
@@ -22,91 +23,66 @@ export const AboutScreenView: FC<AboutScreenProps> = ({ goToCloseBottomSheet }) 
       <View style={globalStyles.topContainer}>
         <ParagraphComponent
           style={globalStyles.headerName}
-          paragraphName={'What is the FlipTok Project?'}
+          paragraphName={strings.getString('about_project')}
         />
         <CloseWindowComponent goToCloseBottomSheet={goToCloseBottomSheet} />
       </View>
       <ScrollView style={aboutScreenStyles.centerContainer}>
         <TextComponent
           style={globalStyles.descriptionText}
-          text={
-            'If you need to download videos from TikTok and you do not know how to do it, we advise you to pay attention to the FlipTok project. FlipTok is a website that allows you to deal with the task in just a few seconds. The service is characterized by a simple and clear interface, which means that it allows you to find and download any videos from TikTok, and will be easy to understand for teenagers and older users. There will be no difficulties because we offer the best solution to save clips on any device.'
-          }
+          text={strings.getString('if_you_need')}
         />
         <ParagraphComponent
           textStyles={globalStyles.paragraphName}
-          paragraphName={'How to use the service?'}
+          paragraphName={strings.getString('how_to_use')}
         />
         <TextComponent
           style={globalStyles.descriptionText}
-          text={
-            'Working with the application involves very simple steps. To download videos and music from TikTok, you only need a few steps. All you need is this:'
-          }
+          text={strings.getString('working_with')}
         />
         <DotComponent
-          text={'go to the app and find a clip you like;'}
+          text={strings.getString('go_to_the_app')}
           style={globalStyles.dotComponent}
         />
         <DotComponent
-          text={'copy the link using the "Share" button;'}
+          text={strings.getString('copy_link')}
           style={globalStyles.dotComponent}
         />
         <DotComponent
-          text={'open the FlipTok application and paste the copied link into the input field.'}
+          text={strings.getString('open_the_app')}
           style={globalStyles.dotComponent}
         />
         <TextComponent
-          text={
-            'The final step is to click the "Download" button and wait for the video to download. It will be found in the "Downloads" or "My Files" folder. Please note that only FlipTok allows you to download videos from TikTok without a watermark, other similar services can\'t do this.\n' +
-            '\n' +
-            'On this website, you can download not only short video clips but also music from TikTok. The algorithm of actions does not change. It is just as easy and simple.\n'
-          }
+          style={globalStyles.dotComponent}
+          text={strings.getString('the_final_step')}
         />
         <ParagraphComponent
           textStyles={globalStyles.paragraphName}
-          paragraphName={'Benefits of FlipTok. What should a user know?'}
+          paragraphName={strings.getString('benefits')}
         />
         <TextComponent
           style={globalStyles.descriptionText}
-          text={
-            'If you have chosen FlipTok for downloading files, you definitely have not made a mistake, because the service has a lot of advantages. The main pros are:'
-          }
+          text={strings.getString('if_you_chosen')}
         />
         <DotComponent
           style={globalStyles.dotComponent}
-          text={
-            'no user fees. You really do not have to pay when downloading clips from TikTok without a watermark, even if there is a lot of content and its size is quite large;'
-          }
+          text={strings.getString('no_user_fees')}
         />
         <DotComponent
           style={globalStyles.dotComponent}
-          text={
-            'high-quality videos. FlipTok allows you to download clips without loss of quality, which means you can watch them on your device in the best possible way;'
-          }
+          text={strings.getString('high_quality')}
         />
         <DotComponent
           style={globalStyles.dotComponent}
-          text={
-            'any video format. We can download videos in MP4, AVI, 3GP and other formats without any problems. In any case, the download will be as fast as possible and the clip will be of high quality;'
-          }
+          text={strings.getString('any_format')}
         />
         <DotComponent
           style={globalStyles.dotComponent}
-          text={
-            'confidentiality. FlipTok will not save user data even if you open the site daily and download a lot of content.'
-          }
+          text={strings.getString('confidentiality')}
         />
         <TextComponent
           style={globalStyles.descriptionText}
-          text={
-            'Using the FlipTok website is not difficult at all, and there is no need to register and authorize. There is no need to download additional extensions, plugins, add-ons, because the website works in any browser.\n' +
-            '\n' +
-            'Another important advantage of the website is the ability to download content to any device. It can be a tablet, a smartphone, or a laptop, in other words, any device on which you usually watch videos.\n' +
-            '\n' +
-            'Downloading is fast, literally in a few seconds for standard-sized videos up to 60 seconds long. Longer videos will take longer to save, but the quality will remain original.\n' +
-            '\n' +
-            'Are you ready to get acquainted with FlipTok service? Hurry up and enjoy videos from TikTok without using the app and internet connection. Our service has already been chosen by hundreds of users of the popular application. It is really the easiest and most convenient way to download clips without watermarking and registration that you and your loved ones are sure to enjoy!'
-          }
+          text={strings.getString('using_the_fliptok')}
         />
       </ScrollView>
     </SafeAreaView>
