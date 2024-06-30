f929433005a81f6f4c27c6ae39edaf0bf90521ef
macwolf <romazverb18@gmsil.com>
2024-06-30 17:28:02 +0300

    added props to closeWindowComponent

--- src/components/closeWindowComponent/closeWindowComponent.tsx
+++ src/components/closeWindowComponent/closeWindowComponent.tsx
@@ -8,14 +8,16 @@ import { closeWindowStyle } from './styles';
 
 export type CloseWindowComponentProps = {
   goToCloseBottomSheet: TouchableOpacityProps['onPress'];
+  style: TouchableOpacityProps['style'];
+  buttonStyle: TouchableOpacityProps['style'];
 };
 
 export const CloseWindowComponent: FC<CloseWindowComponentProps> = (props) => {
-  const { goToCloseBottomSheet } = props;
+  const { goToCloseBottomSheet, style, buttonStyle } = props;
   return (
-    <View style={closeWindowStyle.rootContainer}>
+    <View style={[closeWindowStyle.rootContainer, style]}>
       <TouchableOpacity
-        style={closeWindowStyle.buttonStyle}
+        style={[closeWindowStyle.buttonStyle, buttonStyle]}
         accessibilityRole={'button'}
         onPress={goToCloseBottomSheet}
       >
