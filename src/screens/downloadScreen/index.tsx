bfd85437335fe3bac5cc2bffeced788c1bb74f64
macwolf <romazverb18@gmsil.com>
2024-06-30 18:19:40 +0300

    added deletedFunction, addedProps and states

--- src/screens/downloadScreen/index.tsx
+++ src/screens/downloadScreen/index.tsx
@@ -3,8 +3,13 @@ import { useCallback, useEffect, useState } from 'react';
 import { CameraRoll, PhotoIdentifier } from '@react-native-camera-roll/camera-roll';
 
 import { DownloadScreenView } from './downloadScreenView';
+import { Platform } from 'react-native';
+import ReactNativeBlobUtil from 'react-native-blob-util';
 
 export const DownloadScreen = () => {
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [duration, setDuration] = useState('');
   const [photos, setPhotos] = useState<PhotoIdentifier[] | undefined>([]);
   const fetchPhotos = useCallback(async () => {
     const res = await CameraRoll.getPhotos({
@@ -12,9 +17,22 @@ export const DownloadScreen = () => {
       assetType: 'Videos',
       groupTypes: 'Album',
       groupName: 'FlipTok',
+      include: ['fileSize', 'playableDuration'],
     });
     setPhotos(res?.edges);
   }, []);
+
+  const deleteFile = async () => {
+    Platform.OS === 'android'
+      ? ReactNativeBlobUtil.fs
+          .unlink(url)
+          .then()
+          .catch((err) => {
+            console.log(err);
+          })
+      : await CameraRoll.deletePhotos([url]);
+    setUrl('');
+  };
 
   useEffect(() => {
     fetchPhotos()
@@ -22,5 +40,16 @@ export const DownloadScreen = () => {
       .catch(() => {});
   }, [fetchPhotos, photos]);
 
-  return <DownloadScreenView photos={photos} />;
+  return (
+    <DownloadScreenView
+      photos={photos}
+      url={url}
+      setUrl={setUrl}
+      title={title}
+      setTitle={setTitle}
+      duration={duration}
+      setDuration={setDuration}
+      deleteFile={deleteFile}
+    />
+  );
 };
