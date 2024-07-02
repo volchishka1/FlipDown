4259886429f91ca3221ca296086f9eeb3ccee292
macwolf <romazverb18@gmsil.com>
2024-07-02 17:39:09 +0300

    added backButton function to mainScreen

--- src/screens/mainScreen/index.tsx
+++ src/screens/mainScreen/index.tsx
@@ -1,5 +1,32 @@
 import { MainScreenView } from './mainScreenView';
+import { Alert, BackHandler } from 'react-native';
+import { useEffect } from 'react';
 
 export const MainScreen = () => {
+  useEffect(() => {
+    const onBackPress = () => {
+      Alert.alert(
+        'Exit App',
+        'Do you want to exit?',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => {
+              // Do nothing
+            },
+            style: 'cancel',
+          },
+          { text: 'YES', onPress: () => BackHandler.exitApp() },
+        ],
+        { cancelable: false },
+      );
+
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+    return () => backHandler.remove();
+  }, []);
   return <MainScreenView />;
 };
