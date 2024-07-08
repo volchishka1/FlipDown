894cf77773e7a59ab6f4bb4d9574d503f50e6c5d
macwolf <romazverb18@gmsil.com>
2024-07-08 22:35:53 +0300

    added props and renamed fullVideoScreen component

--- src/stack/DownloadStack.tsx
+++ src/stack/DownloadStack.tsx
@@ -4,15 +4,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { ROUTES } from '@constants/routes';
 import { DownloadScreen } from '@screens/downloadScreen';
-import { FullScreenVideo } from '@components/fullScreenVideo/fullScreenVideoComponent';
+import { MainStackScreenNavigatorParamList } from '@navigation/types';
+import { FullVideoScreen } from '@screens/fullVideoScreen';
 
 export const DownloadStackNavigator = () => {
-  const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator<MainStackScreenNavigatorParamList>();
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false, gestureEnabled: false }}>
       <Stack.Screen name={ROUTES.DOWNLOAD_SCREEN} component={DownloadScreen} />
-      <Stack.Screen name={ROUTES.FULL_VIDEO_SCREEN} component={FullScreenVideo} />
+      <Stack.Screen name={ROUTES.FULL_VIDEO_SCREEN} component={FullVideoScreen} />
     </Stack.Navigator>
   );
 };
