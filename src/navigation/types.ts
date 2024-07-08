2016d235d473b713e05adfbf0cf61cb55ee2b029
macwolf <romazverb18@gmsil.com>
2024-07-08 22:41:11 +0300

    added new route types to paramList

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,8 +1,11 @@
-import { ROUTES } from '../constants/routes';
+import { ROUTES } from '@constants/routes';
 
 export type MainStackScreenNavigatorParamList = {
   [ROUTES.MAIN_SCREEN]: undefined;
   [ROUTES.START_SCREEN]: undefined;
   [ROUTES.SEARCH_SCREEN]: undefined;
+  [ROUTES.FULL_VIDEO_SCREEN]: undefined;
   [ROUTES.DOWNLOAD_SCREEN]: undefined;
+  [ROUTES.DOWNLOAD_STACK_SCREEN]: undefined;
+  [ROUTES.MAIN_STACK_SCREEN]: undefined;
 };
