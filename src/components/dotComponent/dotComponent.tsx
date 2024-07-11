8d7c99c65edd835b175239f991667665275f67c7
macwolf <romazverb18@gmsil.com>
2024-07-12 01:03:40 +0300

    added new props to dotComponent

--- src/components/dotComponent/dotComponent.tsx
+++ src/components/dotComponent/dotComponent.tsx
@@ -8,15 +8,17 @@ import { dotComponentStyles } from './styles';
 
 export interface DotComponentProps {
   text?: TextProps['children'];
+  textStyles?: TextProps['children'];
   style?: TextProps['style'];
+
 }
 export const DotComponent: FC<DotComponentProps> = (props) => {
-  const { text, style } = props;
+  const { text, style, textStyles } = props;
   return (
     <View style={[dotComponentStyles.rootContainer, style]}>
       <View style={dotComponentStyles.dot} />
       <View style={dotComponentStyles.textContainer}>
-        <TextComponent text={text} />
+        <TextComponent textStyles={textStyles} text={text} />
       </View>
     </View>
   );
