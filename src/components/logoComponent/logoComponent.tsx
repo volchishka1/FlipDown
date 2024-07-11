280d8b890498ddf3f1f6bcb8444ebe6df8a72d95
macwolf <romazverb18@gmsil.com>
2024-07-12 01:10:34 +0300

    refactored import in logoComponent

--- src/components/logoComponent/logoComponent.tsx
+++ src/components/logoComponent/logoComponent.tsx
@@ -2,7 +2,7 @@ import { View } from 'react-native';
 
 import React from 'react';
 
-import { LogoSvg } from '../../../assets/logo';
+import { LogoSvg } from '@assets/logo';
 
 import { logoComponentStyles } from './logoComponentStyles';
 
