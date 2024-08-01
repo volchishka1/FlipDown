47694ebf9f3d5b9a90e1499e0a01a7d917c740f5
macwolf <romazverb18@gmsil.com>
2024-08-01 10:15:38 +0300

    added view with screenEmptyText to downloadScreen

--- src/screens/downloadScreen/downloadScreenView.tsx
+++ src/screens/downloadScreen/downloadScreenView.tsx
@@ -1,4 +1,4 @@
-import { Image, SafeAreaView, TouchableOpacity, View } from 'react-native';
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 
 import React, { FC } from 'react';
 
@@ -7,28 +7,37 @@ import { downloadScreenStyles } from './styles';
 import { DownloadScreenProps } from './types';
 import { setUrl } from '@root/store/actions';
 import { useAppDispatch } from '@root/hooks/hooks';
+import { strings } from '@constants';
 
 export const DownloadScreenView: FC<DownloadScreenProps> = (props) => {
   const { photos, navigateToFullVideoScreen } = props;
   const dispatch = useAppDispatch();
   return (
     <SafeAreaView style={downloadScreenStyles.saveAriaView}>
-      <ScrollView style={downloadScreenStyles.rootContainer}>
-        <View style={downloadScreenStyles.centerContainer}>
-          {photos.map((img) => (
-            <TouchableOpacity
-              style={downloadScreenStyles.imageContainer}
-              key={img?.node?.image?.uri}
-              onPress={() => {
-                dispatch(setUrl(img.node.image.uri));
-                navigateToFullVideoScreen();
-              }}
-            >
-              <Image style={downloadScreenStyles.images} source={img.node.image} />
-            </TouchableOpacity>
-          ))}
+      {photos === [] ? (
+        <View style={downloadScreenStyles.textEmptyScreenContainer}>
+          <Text style={downloadScreenStyles.textEmptyScreen}>{`${strings.getString(
+            'screen_is_empty',
+          )}`}</Text>
         </View>
-      </ScrollView>
+      ) : (
+        <ScrollView style={downloadScreenStyles.rootContainer}>
+          <View style={downloadScreenStyles.centerContainer}>
+            {photos.map((img) => (
+              <TouchableOpacity
+                style={downloadScreenStyles.imageContainer}
+                key={img?.node?.image?.uri}
+                onPress={() => {
+                  dispatch(setUrl(img.node.image.uri));
+                  navigateToFullVideoScreen();
+                }}
+              >
+                <Image style={downloadScreenStyles.images} source={img.node.image} />
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
