a9a94bfb0b4a81b7fe8182f7cc161032aca79bb1
macwolf <romazverb18@gmsil.com>
2024-08-20 18:39:15 +0300

    rename props in infoScreenProps

--- src/screens/personalInfoScreen/types.ts
+++ src/screens/personalInfoScreen/types.ts
@@ -5,10 +5,10 @@ import { BottomSheetModal, BottomSheetModalProps } from '@gorhom/bottom-sheet';
 export type InfoScreenViewProps = {
   bottomSheetModalRef?: React.Ref<BottomSheetModal>;
   snapPoints: BottomSheetModalProps['snapPoints'];
-  goToTermsAndConditionsModalScreen: TouchableOpacityProps['onPress'];
-  goToPrivacyPolicyModal: TouchableOpacityProps['onPress'];
   goToContactsModal: TouchableOpacityProps['onPress'];
   goToCloseBottomSheet: TouchableOpacityProps['onPress'];
+  goToTermsAndConditionsLink: TouchableOpacityProps['onPress'];
+  goToPrivacyPolicyLink: TouchableOpacityProps['onPress'];
   goToAboutSheet: TouchableOpacityProps['onPress'];
   actionTriggered: TextProps['children'];
 };
