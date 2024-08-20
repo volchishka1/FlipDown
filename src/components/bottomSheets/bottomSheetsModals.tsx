2f08776760276850b9ea7da7f87c378151009337
macwolf <romazverb18@gmsil.com>
2024-08-20 18:36:05 +0300

    deleted privacyPolicyModal and termsAndConditions modals

--- src/components/bottomSheets/bottomSheetsModals.tsx
+++ src/components/bottomSheets/bottomSheetsModals.tsx
@@ -4,8 +4,6 @@ import { BottomSheetModal } from '@gorhom/bottom-sheet/src';
 
 import { AboutScreenView } from '@screens/aboutScreen/aboutScreenView';
 import { ContactsScreenView } from '@screens/contactsScreen/contactsScreenView';
-import { PrivacyPolicyScreenView } from '@screens/privacyPolicyScreen/privacyPolicyScreenView';
-import { TermsAndConditionComponent } from '@screens/termsAndConditionScreen/termsAndConditionsScreenView';
 import { TextProps, TouchableOpacityProps } from 'react-native';
 
 export type BottomSheetsModalsProps = {
@@ -29,11 +27,7 @@ export const BottomSheetsModals: FC<BottomSheetsModalsProps> = (props) => {
       index={1}
       snapPoints={snapPoints}
     >
-      {actionTriggered === 'termsAndConditionals' ? (
-        <TermsAndConditionComponent goToCloseBottomSheet={goToCloseBottomSheet} />
-      ) : actionTriggered === 'privacyPolicy' ? (
-        <PrivacyPolicyScreenView goToCloseBottomSheet={goToCloseBottomSheet} />
-      ) : actionTriggered === 'contacts' ? (
+      {actionTriggered === 'contacts' ? (
         <ContactsScreenView goToCloseBottomSheet={goToCloseBottomSheet} />
       ) : (
         <AboutScreenView goToCloseBottomSheet={goToCloseBottomSheet} />
