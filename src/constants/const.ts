173486048fae2f7c110a4c460bcf88c064d14a14
macwolf <romazverb18@gmsil.com>
2024-08-20 18:36:19 +0300

    refactored globalConst

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -2,8 +2,3 @@ export enum HttpCode {
   ServerError = 500,
   ClientError = 400,
 }
-
-// export const SET_IS_AUTHORIZED = "SET_IS_AUTHORIZED";
-// export const NO_CHECK_IN_PLUG_TEXT = "No check-in points here";
-// export const ENERGY_IS_OVER = "Energy is over";
-// export const GEODATA_QUERY_INTERVAL = 120000;
