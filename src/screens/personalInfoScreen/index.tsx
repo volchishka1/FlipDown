530e869598af39c56c3ffe1843db4a3c20d133a8
macwolf <romazverb18@gmsil.com>
2024-08-20 18:39:49 +0300

    rename functions in infoScreen

--- src/screens/personalInfoScreen/index.tsx
+++ src/screens/personalInfoScreen/index.tsx
@@ -3,6 +3,9 @@ import { FC, useMemo, useRef, useState } from 'react';
 import { BottomSheetModal } from '@gorhom/bottom-sheet/src';
 
 import { InfoScreenView } from './infoScreenView';
+import { Alert, Linking } from 'react-native';
+import { NetInfoState, useNetInfo } from '@react-native-community/netinfo';
+import { strings } from '@constants';
 
 export type InfoScreenProps = {};
 export const InfoScreen: FC<InfoScreenProps> = (props) => {
@@ -10,14 +13,9 @@ export const InfoScreen: FC<InfoScreenProps> = (props) => {
   const [actionTriggered, setActionTriggered] = useState('');
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
   const snapPoints = useMemo(() => ['90%', '90%'], []);
-  const goToPrivacyPolicyModal = () => {
-    bottomSheetModalRef.current?.present();
-    setActionTriggered('privacyPolicy');
-  };
-  const goToTermsAndConditionalsModal = () => {
-    bottomSheetModalRef.current?.present();
-    setActionTriggered('termsAndConditionals');
-  };
+
+  const internetState: NetInfoState = useNetInfo();
+
   const goToContactsModal = () => {
     bottomSheetModalRef.current?.present();
     setActionTriggered('contacts');
@@ -32,16 +30,39 @@ export const InfoScreen: FC<InfoScreenProps> = (props) => {
     bottomSheetModalRef.current?.close();
   };
 
+  const goToPrivacyPolicyLink = () => {
+    if (internetState.isConnected === false) {
+      Alert.alert(`${strings.getString('no_internet')}`, `${strings.getString('sorry')}`, [
+        { text: `${strings.getString('okay')}` },
+      ]);
+    } else {
+      Linking.openURL('https://fliptok.app/mobile-privacy-policy').catch((err) =>
+        console.error('An error occurred', err),
+      );
+    }
+  };
+  const goToTermsAndConditionsLink = () => {
+    if (internetState.isConnected === false) {
+      Alert.alert(`${strings.getString('no_internet')}`, `${strings.getString('sorry')}`, [
+        { text: `${strings.getString('okay')}` },
+      ]);
+    } else {
+      Linking.openURL('https://fliptok.app/mobile-terms-and-conditions').catch((err) =>
+        console.error('An error occurred', err),
+      );
+    }
+  };
+
   return (
     <InfoScreenView
       bottomSheetModalRef={bottomSheetModalRef}
       snapPoints={snapPoints}
       actionTriggered={actionTriggered}
-      goToPrivacyPolicyModal={goToPrivacyPolicyModal}
-      goToTermsAndConditionalsModal={goToTermsAndConditionalsModal}
       goToContactsModal={goToContactsModal}
       goToCloseBottomSheet={goToCloseBottomSheet}
       goToAboutSheet={goToAboutSheet}
+      goToPrivacyPolicyLink={goToPrivacyPolicyLink}
+      goToTermsAndConditionsLink={goToTermsAndConditionsLink}
     />
   );
 };
