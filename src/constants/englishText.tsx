6b7da1d34d72b0b6b2563851435cf8a0aec8f6f1
macwolf <romazverb18@gmsil.com>
2024-09-11 20:11:00 +0300

    refactored english and russian text

--- src/constants/englishText.tsx
+++ src/constants/englishText.tsx
@@ -21,7 +21,7 @@ export const ENTER_YOUR_LINK = 'Enter your link';
 export const YES = 'Yes';
 export const NO = 'No';
 export const DO_YOU_WANT_TO_DELETE_FILE = 'Do you want to delete file';
-export const INSERT_YOUR_LINK = 'Insert TikTok link';
+export const INSERT_YOUR_LINK = 'Insert TT link';
 export const DO_YOU_WANT_TO_EXIT = 'Do you want to exit';
 export const EXIT_APP = 'Exit app';
 export const VIDEO_DELETED = 'Video deleted';
@@ -30,10 +30,10 @@ export const SCREEN_IS_EMPTY = 'Download screen is empty';
 // ABOUT STRINGS
 export const ABOUT_PROJECT = 'What is the FlipTok Project?';
 export const IF_YOU_NEED =
-  'If you need to download videos from TikTok and you do not know how to do it, we advise you to pay attention to the FlipTok project. FlipTok is an application that allows you to deal with the task in just a few seconds. The service is characterized by a simple and clear interface, which means that it allows you to find and download any videos from TikTok, and will be easy to understand for teenagers and older users. There will be no difficulties because we offer the best solution to save clips on any device.';
+  'If you need to download videos from TT and you do not know how to do it, we advise you to pay attention to the FlipTok project. FlipTok is an application that allows you to deal with the task in just a few seconds. The service is characterized by a simple and clear interface, which means that it allows you to find and download any videos from TT, and will be easy to understand for teenagers and older users. There will be no difficulties because we offer the best solution to save clips on any device.';
 export const HOW_TO_USE = 'How to use the service?';
 export const WORKING_WITH =
-  'Working with the application involves very simple steps. To download videos and music from TikTok, you only need a few steps. All you need is this:';
+  'Working with the application involves very simple steps. To download videos and music from TT, you only need a few steps. All you need is this:';
 export const GO_TO_THE_APP = 'go to the app and find a clip you like;';
 export const COPY_LINK = 'copy the link using the "Share" button;';
 export const OPEN_THE_APP =
@@ -41,12 +41,12 @@ export const OPEN_THE_APP =
 export const THE_FINAL_STEP =
   'The final step is to click the "Download" button and wait for the video to download. It will be found in the "Downloads" or "Gallery" folder.\n' +
   '\n' +
-  'On this application, you can download not only short video clips but also music from TikTok. The algorithm of actions does not change. It is just as easy and simple.\n';
+  'On this application, you can download not only short video clips but also music from TT. The algorithm of actions does not change. It is just as easy and simple.\n';
 export const BENEFITS = 'Benefits of FlipTok. What should a user know?';
 export const IF_YOU_CHOSEN =
   'If you have chosen FlipTok for downloading files, you definitely have not made a mistake, because the service has a lot of advantages. The main pros are:';
 export const NO_USER_FEES =
-  'no user fees. You really do not have to pay when downloading clips from TikTok without a watermark, even if there is a lot of content and its size is quite large;';
+  'no user fees. You really do not have to pay when downloading clips from TT without a watermark, even if there is a lot of content and its size is quite large;';
 export const HIGH_QUALITY =
   'high-quality videos. FlipTok allows you to download clips without loss of quality, which means you can watch them on your device in the best possible way;';
 export const ANY_FORMAT =
@@ -60,13 +60,13 @@ export const USING_THE_FLIPTOK =
   '\n' +
   'Downloading is fast, literally in a few seconds for standard-sized videos up to 60 seconds long. Longer videos will take longer to save, but the quality will remain original.\n' +
   '\n' +
-  'Are you ready to get acquainted with FlipTok service? Hurry up and enjoy videos from TikTok without using the app and internet connection. Our service has already been chosen by hundreds of users of the popular application. It is really the easiest and most convenient way to download clips without watermarking and registration that you and your loved ones are sure to enjoy!';
+  'Are you ready to get acquainted with FlipTok service? Hurry up and enjoy videos from TT without using the app and internet connection. Our service has already been chosen by hundreds of users of the popular application. It is really the easiest and most convenient way to download clips without watermarking and registration that you and your loved ones are sure to enjoy!';
 
 // CONTACTS STRINGS
 
 export const CONTACT_US = 'Contact us';
 export const YOU_CAN_CONTACT_US = 'You can contact us using the following contact data:';
-export const EMAIL = 'Email: fliptokapp@gmail.com';
+export const EMAIL = 'Email: flipapptechnology@gmail.com';
 export const IF_YOU_HAVE_QUESTIONS =
   'If you have questions, suggestions or comments, please do not hesitate to contact us. We are always ready to help you and answer all your questions.';
 export const WE_VALUE_YOUR_ATTENTION =
