a6c0470040557d122a8d0cd4fe935667355c7ecc
macwolf <romazverb18@gmsil.com>
2024-10-31 12:29:31 +0300

    rescheduled useEffect for hid splashScreen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,13 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 
-import { Navigator } from './src/navigation/Navigator';
+import { Navigator } from '@navigation/Navigator';
 import { store } from './src/store/store';
-import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
   return (
     <Provider store={store}>
       <SafeAreaProvider>
