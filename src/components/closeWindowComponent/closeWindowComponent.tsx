172367086e68e1a0e6bae6eee63f140bb45d2dae
macwolf <romazverb18@gmail.com>
2024-10-31 23:24:20 +0300

    changed versionCode and name, and rename accessibilityLabel

--- src/components/closeWindowComponent/closeWindowComponent.tsx
+++ src/components/closeWindowComponent/closeWindowComponent.tsx
@@ -17,7 +17,7 @@ export const CloseWindowComponent: FC<CloseWindowComponentProps> = (props) => {
     <View style={[closeWindowStyle.rootContainer, style]}>
       <TouchableOpacity
         style={[closeWindowStyle.buttonStyle, buttonStyle]}
-        accessibilityLabel={'Close button'}
+        accessibilityLabel={'Close Window'}
         onPress={goToCloseButton}
       >
         {iconSvg}
